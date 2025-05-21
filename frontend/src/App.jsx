6ac1645ca86a4674a69ea7ec49cef2ea5e971866
Clement Delphin <clementcla@outlook.com>
2025-05-21 18:20:27 +0530

    Intial commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl bg-purple-500 flex items-center justify-center min-h-screen'>Hello welcome to my shorts video</div>
+  )
+}
+
+export default App
