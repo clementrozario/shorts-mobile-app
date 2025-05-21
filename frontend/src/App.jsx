76fefc9bc66a2798c3d03e912643f3af283fdd22
Clement Delphin <clementcla@outlook.com>
2025-05-21 18:26:56 +0530

    tested tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-2xl bg-purple-500 flex items-center justify-center min-h-screen'>Hello welcome to my shorts video</div>
+    <div className='text-2xl bg-purple-500 flex items-center justify-center min-h-screen'>
+      Hello welcome to my shorts video
+      </div>
   )
 }
 
